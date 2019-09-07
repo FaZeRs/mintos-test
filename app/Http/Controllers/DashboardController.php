b3a99be069a42d3b23398de4a1bba979a0495945
Nauris Linde <naurislinde@gmail.com>
2019-09-07 18:38:43 +0300

    Added RSS feed to Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Inertia\Inertia;
+use SimplePie;
 
 class DashboardController extends Controller
 {
     public function __invoke()
     {
-        return Inertia::render('Dashboard/Index');
+        $feed = new SimplePie();
+        $feed->set_item_class();
+        $feed->enable_cache(true);
+        $feed->set_cache_duration(3600);
+        $feed->set_cache_location(storage_path('framework/cache'));
+        $feed->set_feed_url('https://www.theregister.co.uk/software/headlines.atom');
+        $feed->init();
+        $feed->handle_content_type();
+
+        $feedItems = collect($feed->get_items())->map(function (\SimplePie_Item $item) {
+            return [
+                'title' => $item->get_title(),
+                'description' => strip_tags($item->get_description()),
+                'date' => Carbon::parse($item->get_date())->diffForHumans(),
+                'link' => $item->get_link(),
+            ];
+        });
+
+        return Inertia::render('Dashboard/Index', [
+            'feedItems' => $feedItems
+        ]);
     }
 }
--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -1,15 +1,25 @@
 <template>
     <layout title="Dashboard">
         <h1 class="mb-8 font-bold text-3xl">Dashboard</h1>
+        <section class="shadow">
+            <div v-for="(item, key) in feedItems" :key="key">
+                <accordion :item="item"></accordion>
+            </div>
+        </section>
     </layout>
 </template>
 
 <script>
     import Layout from '@/Shared/Layout'
+    import Accordion from '@/Shared/Accordion'
 
     export default {
         components: {
             Layout,
+            Accordion
+        },
+        props: {
+            feedItems: Array,
         },
     }
 </script>
