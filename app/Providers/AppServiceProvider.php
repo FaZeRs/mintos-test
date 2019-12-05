2072dd71d259dd4b2075c5e7846a572cea06e1d8
Nauris Linde <naurislinde@gmail.com>
2019-12-05 09:55:45 +0000

    Apply fixes from StyleCI

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,13 +2,13 @@
 
 namespace App\Providers;
 
-use Inertia\Inertia;
 use Carbon\CarbonImmutable;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\ServiceProvider;
+use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
